bb1d639777dc8f4859c467c1fb896a3565510b16
Jcobi <diacob569@gmail.com>
2021-02-17 11:35:31 -0800

    test 1

---
+++ Modul1/Modul1_.py
@@ -0,0 +1,6 @@
+# print shape
+print("*******") ; print("**") ; print("**")
+print("**")      ; print("**") ; print("**")
+print("*******") ; print("**") ;print("**")
+print("**")      ; print("**") ; print("**")
+print("*******") ; print("*****************")
